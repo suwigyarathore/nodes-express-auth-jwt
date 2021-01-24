2302753a66d528564a976ed1a385bcb9af527980
Suwigya Rathore <suwigya@kramphub.com>
2021-01-24 22:50:41 +0100

    Finishing up login

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
@@ -25,6 +24,19 @@ userSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
 const User = mongoose.model('user', userSchema);
 
